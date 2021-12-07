799d55572c3cba2db3db0dac240aff6c27bf89ac
KyleDanny <codingkyle92@gmail.com>
2021-12-07 09:13:56 +0100

    cellphone routes

--- CoreCellphone.js
+++ CoreCellphone.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const coreCellphoneSchema = new Schema({
+  cellphoneID: String,
   name: String,
   imei: String,
   type: String,
+  comments: String,
 })
 
 const coreCellphone = mongoose.model('coreCellphone', coreCellphoneSchema, 'CoreCellphones');
